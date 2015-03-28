99d588871d5de22e764479c30c130b6e2b0f22b8
siscodr <siscodr@rose-hulman.edu>
2015-03-27 20:24:02 -0400

    Test two fails!
    Made a new test, that fails

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/28/90edb4e6dfd400141e74f265530e9a20
@@ -0,0 +1,20 @@
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+
+
+public class PrimeFactorizationTest {
+
+	@Test
+	public void Test1() {
+		assertEquals(list(),PrimeFactors.generate(1));
+	}
+	
+	public List<Integer> list(){
+		return Arrays.asList();
+	}
+
+}
--- PrimeFactors/src/PrimeFactorizationTest.java
+++ PrimeFactors/src/PrimeFactorizationTest.java
@@ -3,18 +3,24 @@ import static org.junit.Assert.*;
 import java.util.Arrays;
 import java.util.List;
 
+import junit.framework.TestCase;
+
 import org.junit.Test;
 
 
 public class PrimeFactorizationTest {
 
+	private List<Integer> list(Integer...integers){
+		return Arrays.asList(integers);
+	}
+	
 	@Test
-	public void Test1() {
+	public void testone() {
 		assertEquals(list(),PrimeFactors.generate(1));
 	}
 	
-	public List<Integer> list(){
-		return Arrays.asList();
+	@Test
+	public void testtwo() throws Exception {
+		assertEquals(list(2),PrimeFactors.generate(2));
 	}
-
 }
